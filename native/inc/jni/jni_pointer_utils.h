73eabaf6e3243168b5fb67b33933948c718832b2
waicool20 <waicool20@gmail.com>
2017-09-14 01:06:28 +0800

    Providing static and non static variants for pointer utils

--- native/inc/jni/jni_pointer_utils.h
+++ native/inc/jni/jni_pointer_utils.h
@@ -5,6 +5,16 @@
 
 template<typename T>
 optional<T*> PointerFromCPointer(JNIEnv* env, jobject obj) {
+    auto CPointer = GetObjectFieldValue(env, obj, "handle", "com.waicool20.skrypton.jni.CPointer");
+    if (CPointer) {
+        auto longPtr = GetFieldValue<jlong>(env, CPointer.value(), "handle");
+        if (longPtr) return { (T*) longPtr.value() };
+    }
+    return {};
+}
+
+template<typename T>
+optional<T*> PointerFromStaticCPointer(JNIEnv* env, jobject obj) {
     auto CPointer = GetStaticObjectFieldValue(env, obj, "handle", "com.waicool20.skrypton.jni.CPointer");
     if (CPointer) {
         auto longPtr = GetFieldValue<jlong>(env, CPointer.value(), "handle");
@@ -16,6 +26,11 @@ optional<T*> PointerFromCPointer(JNIEnv* env, jobject obj) {
 template<typename T>
 T DeleteCPointerReference(JNIEnv* env, jobject obj) {
     delete PointerFromCPointer<T>(env, obj);
+}
+
+template<typename T>
+T DeleteStaticCPointerReference(JNIEnv* env, jobject obj) {
+    delete PointerFromStaticCPointer<T>(env, obj);
 }
 
 #endif //SKRYPTONNATIVE_JNI_POINTER_UTILS_H
