87c85cb28a6ccae5557cb3d94ba2d6b9580d4879
waicool20 <waicool20@gmail.com>
2017-09-24 22:29:40 +0800

    Suprressing warnings for SKryptonMatch

--- src/main/kotlin/com/waicool20/skrypton/sikulix/SKryptonMatch.kt
+++ src/main/kotlin/com/waicool20/skrypton/sikulix/SKryptonMatch.kt
@@ -2,4 +2,5 @@ package com.waicool20.skrypton.sikulix
 
 import org.sikuli.script.Match
 
+@Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class SKryptonMatch(match: Match, screen: SKryptonScreen): Match(match), SikuliInterface by SKryptonRegion(match, screen)
