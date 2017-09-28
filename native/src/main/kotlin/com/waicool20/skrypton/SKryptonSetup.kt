58c1c8e3e799b7331b1a52e50ac7f94afd396573
waicool20 <waicool20@gmail.com>
2017-09-28 15:26:10 +0800

    Added setup logic to native jar to unpack all the resources to .skrypton directory

---
+++ native/src/main/kotlin/com/waicool20/skrypton/SKryptonSetup.kt
@@ -0,0 +1,106 @@
+package com.waicool20.skrypton
+
+import org.slf4j.LoggerFactory
+import java.io.InputStream
+import java.net.URI
+import java.nio.file.*
+import java.nio.file.attribute.PosixFilePermission
+
+fun main(args: Array<String>) {
+    SKryptonSetup.start()
+}
+
+object SKryptonSetup {
+    val logger = LoggerFactory.getLogger(SKryptonSetup::class.java)
+    val javaHome = Paths.get(System.getProperty("java.home"))
+    val skryptonAppDir = Paths.get(System.getProperty("user.home")).resolve(".skrypton")
+    val codeSource = javaClass.protectionDomain.codeSource.location.toURI().path
+
+    fun start() {
+        logger.debug("Starting setup")
+        Files.createDirectories(skryptonAppDir)
+        copy(javaHome.resolve("bin/java"), skryptonAppDir.resolve("bin/java"), true)
+        symlink(javaHome.resolve("lib"), skryptonAppDir.resolve("lib"))
+        if (codeSource.endsWith(".jar")) {
+            val jarURI = URI.create("jar:file:$codeSource")
+            val env = mapOf(
+                    "create" to "false",
+                    "encoding" to "UTF-8"
+            )
+            (FileSystems.newFileSystem(jarURI, env)).use { fs ->
+                Files.walk(fs.getPath("/skrypton"))
+                        .filter { it.nameCount > 1 }
+                        .forEach(SKryptonSetup::copyResource)
+            }
+        } else {
+            // For use in IDE
+            Files.walk(Paths.get("native/build/resources/main/skrypton"))
+                    .filter { it.nameCount > 1 }
+                    .forEach(SKryptonSetup::copyResource)
+        }
+        logger.debug("Done!")
+    }
+
+    private fun copyResource(resource: Path) {
+        val dest = skryptonAppDir.resolve("bin/${resource.subpath(1, resource.nameCount)}")
+        copy(resource, dest)
+        if (OS.isUnix()) {
+            val perms = Files.getPosixFilePermissions(dest).toMutableSet()
+            perms.addAll(listOf(PosixFilePermission.OWNER_EXECUTE))
+            Files.setPosixFilePermissions(dest, perms)
+        }
+    }
+
+    private fun <T> copy(source: T, target: Path, overwrite: Boolean = false) {
+        if (Files.notExists(target)) {
+            Files.createDirectories(target.parent)
+            when (source) {
+                is Path -> {
+                    logger.debug("[COPY] $source to $target")
+                    if (overwrite) {
+                        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING)
+                    } else {
+                        Files.copy(source, target)
+                    }
+                }
+                is InputStream -> {
+                    logger.debug("[COPY] $target")
+                    if (overwrite) {
+                        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING)
+                    } else {
+                        Files.copy(source, target)
+                    }
+                }
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
+
+    private fun symlink(source: Path, target: Path) {
+        if (Files.notExists(target)) {
+            Files.createDirectories(target.parent)
+            Files.createSymbolicLink(target, source)
+            logger.debug("[SYMLINK] $target to $source")
+        }
+    }
+}
+
+object OS {
+    fun is32Bit() = !is64Bit()
+    fun is64Bit() = System.getProperty("os.arch").contains("64")
+    fun isWindows() = System.getProperty("os.name").toLowerCase().contains("win")
+    fun isLinux() = System.getProperty("os.name").toLowerCase().contains("linux")
+    fun isMac() = System.getProperty("os.name").toLowerCase().contains("mac")
+
+    fun isUnix() = !isWindows()
+    fun isDos() = isWindows()
+
+    val libraryExtention by lazy {
+        when {
+            isLinux() -> ".so"
+            isWindows() -> ".dll"
+            isMac() -> ".dylib"
+            else -> throw Exception("Unknown OS")
+        }
+    }
+}
