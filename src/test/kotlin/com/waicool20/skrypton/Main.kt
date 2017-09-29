2633280f5d536dae3fafc28015d197b9ad6706b6
waicool20 <waicool20@gmail.com>
2017-09-29 20:59:49 +0800

    Changed test to a more interesting drawing demo

--- src/test/kotlin/com/waicool20/skrypton/Main.kt
+++ src/test/kotlin/com/waicool20/skrypton/Main.kt
@@ -3,26 +3,45 @@ package com.waicool20.skrypton
 import com.waicool20.skrypton.jni.objects.SKryptonApp
 import com.waicool20.skrypton.jni.objects.SKryptonWebView
 import com.waicool20.skrypton.sikulix.SKryptonScreen
+import org.sikuli.script.ImagePath
 import org.sikuli.script.Location
+import java.awt.Rectangle
+import java.util.concurrent.ThreadLocalRandom
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    ImagePath.add(ClassLoader.getSystemClassLoader().getResource("images"))
     val app = SKryptonApp.initalize(remoteDebugPort = 8888)
-    val webView = SKryptonWebView("https://www.google.com")
+    val webView = SKryptonWebView("http://www.onemotion.com/flash/sketch-paint/")
+    webView.settings.pluginsEnabled = true
     webView.resize(1280, 720)
     webView.show()
     webView.showCursor = true
     val screen = SKryptonScreen(webView)
     thread {
-        TimeUnit.SECONDS.sleep(2)
-        println(webView.url)
-    }
-    thread {
-        TimeUnit.SECONDS.sleep(5)
+        TimeUnit.SECONDS.sleep(3)
         println("Sending events!")
-        screen.click(Location(670, 400))
+        screen.click("ok1.png")
+        TimeUnit.SECONDS.sleep(1)
+        screen.click("ok2.png")
+        TimeUnit.SECONDS.sleep(1)
+        screen.click("size.png")
+        val rng = ThreadLocalRandom.current()
+        val drawingArea = Rectangle(195, 65, 1020, 590)
+        while (true) {
+            val location1 = Location(
+                    rng.nextInt(drawingArea.x, drawingArea.x + drawingArea.width),
+                    rng.nextInt(drawingArea.y, drawingArea.y + drawingArea.height)
+            )
+            val location2 = Location(
+                    rng.nextInt(drawingArea.x, drawingArea.x + drawingArea.width),
+                    rng.nextInt(drawingArea.y, drawingArea.y + drawingArea.height)
+            )
+            println("Drawing line from $location1 to $location2")
+            screen.dragDrop(location1, location2)
+        }
     }
     exitProcess(app.exec())
 }
