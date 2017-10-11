d8c3171297818079b80509bec7d5796c2a0a3e18
waicool20 <waicool20@gmail.com>
2017-10-11 13:55:02 +0800

    Adding docs for SKryptonApp

--- src/main/kotlin/com/waicool20/skrypton/jni/objects/SKryptonApp.kt
+++ src/main/kotlin/com/waicool20/skrypton/jni/objects/SKryptonApp.kt
@@ -35,13 +35,22 @@ import java.nio.file.Paths
 import kotlin.streams.toList
 import kotlin.system.exitProcess
 
+/**
+ * A singleton app instance for this library and is the main entry point for any program that wants
+ * to use any of the SKrypton APIs. This object is in charge of initializing the native components
+ * and managing it.
+ */
 object SKryptonApp : NativeInterface() {
+    /**
+     * Holds a [Path] pointing to the directory used by SKrypton, this directory contains all the
+     * native libraries and resources that SKrypton will use.
+     */
     val APP_DIRECTORY: Path = Paths.get(System.getProperty("user.home")).resolve(".skrypton")
 
     private val logger = loggerFor<SKryptonApp>()
     override lateinit var handle: CPointer
 
-    // Load order
+    // Get the load order from the nativeLibraries-<OS> file in resources
     private val nativeDependencies = run {
         val resourceFile = when {
             OS.isLinux() -> "nativeLibraries-linux.txt"
@@ -68,6 +77,16 @@ object SKryptonApp : NativeInterface() {
         }
     }
 
+    /**
+     * Initializes the native components.
+     *
+     * @param args Arguments to pass to the native components, recommended to just pass the array
+     * received from the main function. Default is an empty array.
+     * @param remoteDebugPort The remote debug port is initialized if given a valid value from 0 to 65535
+     * and that the port is not previously occupied. Default is -1 (Not initialized).
+     * @param action Action to be executed with the SKryptonApp instance as its receiver.
+     * @return [SKryptonApp] instance (Can be used to chain [exec] function)
+     */
     fun initialize(args: Array<String> = emptyArray(),
                    remoteDebugPort: Int = -1,
                    action: SKryptonApp.() -> Unit = {}
@@ -80,16 +99,42 @@ object SKryptonApp : NativeInterface() {
         return this
     }
 
+    /**
+     * Blocking function which starts the execution of the SKryptonApp.
+     *
+     * @param exit Whether or not to exit when SKryptonApp instance is done. Defaults to false
+     * @return If [exit] is false, this function will return the exit code of the native side.
+     */
     fun exec(exit: Boolean = false): Int {
         val exitCode = exec_N()
         if (exit) exitProcess(exitCode)
         return exitCode
     }
+
+    /**
+     * Executes the given action on the thread where SKryptonApp exists. This is similar to the
+     * [javafx.application.Platform.runLater] method.
+     *
+     * @param action Action to be executed
+     */
     fun runOnMainThread(action: () -> Unit) = runOnMainThread_N(Runnable { action() })
 
     //<editor-fold desc="Environment functions">
 
+    /**
+     * Puts an variable into the native environment, it is lost when the SKryptonApp is done executing
+     *
+     * @param key The name of the environment variable
+     * @param value The value of the environment variable
+     */
     fun putEnv(key: String, value: String) = putEnv_N(key, value)
+
+    /**
+     * Gets the value of the native environment variable
+     *
+     * @param key The name of the environment variable
+     * @return The value of the environment variable
+     */
     fun getEnv(key: String) = getEnv_N(key)
 
     //</editor-fold>
